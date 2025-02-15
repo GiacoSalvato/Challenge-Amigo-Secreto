e6a87daac17fc51eca99d6fcf6e830460da924c0
Giacomo <giacosalvato@gmail.com>
2025-02-15 17:52:50 -0300

    actualiza la condicion if para que no agreguen numeros

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+
 function agregarAmigo(nombre){
-    let amigos =document.getElementById("amigo").value;
+    let amigos = document.getElementById("amigo").value;
     console.log(amigos);
 
-    if(amigos == ""){
-        alert("Debe ingresar un nombre de amigo");
+    if(amigos == "" || amigos > 0){
+        alert("Por favor, inserte un nombre");
+        limpiarCaja();
 } else {
     limpiarCaja(); 
  }
