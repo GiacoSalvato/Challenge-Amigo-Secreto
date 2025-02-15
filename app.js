ff35fe6ba2a45267310518c68577c5e8aba6cc1e
Giacomo <giacosalvato@gmail.com>
2025-02-15 16:33:21 -0300

    Agrega la funcion agregarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(nombre){
+    let amigos =document.getElementById("amigo").value;
+    console.log(amigos);
+}
+
+
+
+
+
+
+
