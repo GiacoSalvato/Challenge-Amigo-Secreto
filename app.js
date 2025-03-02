bf222f079749fbf5277fda5dcc82f0058320c04c
Giacomo <giacosalvato@gmail.com>
2025-03-02 01:34:55 -0300

    se agrega funcion para enfocar en cuadro y usar tecla enter

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+enfocar();
+manejarEnter();
 
 //función principal
 //agregar un amigo a la lista
  function agregarAmigo() {
     let nombre = document.getElementById("amigo").value.trim();
     console.log(nombre);
-
-    if (nombre.trim() === "" || !isNaN(nombre)) { // Verificamos si esta vacío o con número
+// Verificamos si esta vacío o con número o con caracteres especiales
+    if (nombre.trim() === "" || !isNaN(nombre) || !/^[a-zA-ZÀ-ÿ\s]+$/.test(nombre)) { 
         agregarTextoElemento('h2', 'Ingrese un nombre de amigo válido');
         limpiarCaja();
-        return;
-    }
-
-    if (!/^[a-zA-ZÀ-ÿ\s]+$/.test(nombre)) { // Verificamos si solo tiene letras
-        agregarTextoElemento('h2', 'Ingrese un nombre válido (solo letras)');
         return;
     }
 
@@ -27,6 +24,7 @@ let amigos = [];
     }
     limpiarCaja();
     actualizarLista();
+    enfocar();
     return;
 }
 
@@ -56,6 +54,7 @@ function sortearAmigo(){
     if(amigos.length == 0){
         agregarTextoElemento('h2', 'No hay amigos para sortear');
         actualizarLista();
+        enfocar();
         return;
     }
     console.log(amigoSorteado);
@@ -84,6 +83,20 @@ function limpiarCaja(){
     return;
 }
 
+// se agrega la función enfocar con ayuda de focus, para que el cursor esté dentro del input
+function enfocar () {
+    document.getElementById('amigo').focus();
+    return;
+}
 
+//función para manejar el evento de presionar la tecla Enter
+function manejarEnter(){
+    document.getElementById("amigo").addEventListener("keyup", function(event){
+        if(event.key === "Enter"){
+            agregarAmigo();
+        }
+    });
+    return;
+}
 
 
